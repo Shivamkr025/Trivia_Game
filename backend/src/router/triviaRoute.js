97bee843598ebbe89d27290910251d305a7883f6
Shivamkr025 <shivam22@navgurukul.org>
2024-09-12 09:29:49 +0530

    add all router

---
+++ backend/src/router/triviaRoute.js
@@ -0,0 +1,15 @@
+import express from 'express'
+import { createPlayer } from '../controller/player.js';
+import { createQuestion, showAllQuestion } from '../controller/question.js';
+
+const router = express.Router()
+
+router.post('/create/question' , createQuestion)
+
+router.get('/show/question' , showAllQuestion)
+
+router.post('/create/player' , createPlayer)
+
+// router.post('/category/select' , categorySelect)
+
+export default router
