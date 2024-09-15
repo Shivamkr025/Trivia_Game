6d6839fb3ef38905ed7efcb1aedbc06981b846fd
Shivamkr025 <shivam22@navgurukul.org>
2024-09-15 21:57:27 +0530

    update some function

--- backend/model/gameModel.js
+++ backend/model/gameModel.js
@@ -1,11 +1,7 @@
 import mongoose from "mongoose";
 
-const nameSchema = new mongoose.Schema({
-    name: { type: String },
-    score: { type: Number, default: 0 }
-})
-
 const questionSchema = new mongoose.Schema({
+
     questionId: { type: String },
 
     category: { type: String },
@@ -22,22 +18,8 @@ const questionSchema = new mongoose.Schema({
     correctAnswer: { type: String },
 })
 
-const gameSchema = new mongoose.Schema({
-    
-    players: [{ type: String }],
-    selectDifficulty: [{ type: String }],
-    checkQuestion: [{
-        questionId: { type: mongoose.Schema.Types.ObjectId },
-        answered: { type: mongoose.Schema.Types.ObjectId },
-        correct: { type: Boolean }
-    }],
-
-    winner: { type: mongoose.Schema.Types.ObjectId }
-
-})
-
 const Player = mongoose.model('player', nameSchema)
-const Question = mongoose.model('Question', questionSchema)
-const Game = mongoose.model('Game', gameSchema)
 
-export { Player, Question, Game }
+const Question = mongoose.model('Question', questionSchema)
+
+export { Player, Question }
--- backend/router/triviaRoute.js
+++ backend/router/triviaRoute.js
@@ -1,5 +1,4 @@
 import express from 'express'
-import { createPlayer } from '../controller/player.js';
 import { createQuestion, showAllQuestion } from '../controller/question.js';
 
 const router = express.Router()
@@ -7,9 +6,5 @@ const router = express.Router()
 router.post('/create/question', createQuestion)
 
 router.get('/show/question', showAllQuestion)
-
-router.post('/create/player', createPlayer)
-
-// router.post('/category/select' , categorySelect)
 
 export default router
